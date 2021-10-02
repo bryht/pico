e7f3caa2350f7cc25f0637a9a15eac74b04f87d6
Ming Li <ming.li@dephion.com>
2021-10-02 12:19:21 +0200

    wip

--- led-flash.py
+++ led-flash.py
@@ -1,8 +1,10 @@
 import machine
 import utime
 
-led_onboard = machine.Pin(25, machine.Pin.OUT)
+led_onboard = machine.Pin(25, machine.Pin.OUT);
+led_external = machine.Pin(15, machine.Pin.OUT);
 
 while True:
- led_onboard.toggle()
+ led_onboard.on()
+ led_external.off()
  utime.sleep(1)
